676d255425c121283e127c5ddce8a83807656ad9
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:26:25 -0700

    Add the department class hire method

--- lib/department.rb
+++ lib/department.rb
@@ -1,8 +1,14 @@
+require './lib/employee'
+
 class Department
   attr_reader :name, :employees
 
   def initialize(name)
     @name = name
     @employees = []
+  end
+
+  def hire(employee)
+    @employees << employee
   end
 end
