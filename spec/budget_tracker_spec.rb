eab21afeb9e094db01ece3eae40a3fb46054337a
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:43:55 -0700

    Add test for budgettracker class instance variable departments

--- spec/budget_tracker_spec.rb
+++ spec/budget_tracker_spec.rb
@@ -3,9 +3,15 @@ require './lib/employee'
 require './lib/budget_tracker'
 
 describe 'budgettracker' do
-  it "exsits" do
+  it "exists" do
     budget = BudgetTracker.new("2000")
 
     expect(budget).to be_an_instance_of(BudgetTracker)
+  end
+
+  it "does not have any departments by default" do
+    budget = BudgetTracker.new("2000")
+
+    expect(budget.departments).to eq([])
   end
 end
