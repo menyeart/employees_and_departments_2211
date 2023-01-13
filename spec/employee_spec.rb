52f619e9be0b392b3379027b6e58d213d094b132
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:07:44 -0700

    Add a test for the employee name attribute

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,9 +3,16 @@ require './lib/employee'
 describe 'employee' do
   it "exists" do
     bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
-    
+
     expect(bobbi).to be_an_instance_of(Employee)
   end
+
+  it "has a name" do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    
+    expect(bobbi.name).to eq("Bobbi Jaeger")
+  end
+
 
 
 
