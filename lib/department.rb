0c08d543182fabeb99aa767587d2ab42e483376e
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 10:22:32 -0700

    Add employee expenses instance variable

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,16 @@
 require './lib/employee'
 
 class Department
-  attr_reader :name, :employees, :expenses
+  attr_reader :name, 
+              :employees, 
+              :expenses, 
+              :employee_expenses
 
   def initialize(name)
     @name = name
     @employees = []
     @expenses = 0
+    @employee_expenses = []
   end
 
   def hire(employee)
