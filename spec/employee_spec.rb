ece92696d982f5ca6a7c50c8614137b967610641
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:11:32 -0700

    Add a test for the employee salary attribute

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -19,6 +19,12 @@ describe 'employee' do
     expect(bobbi.age).to eq(30)
   end
 
+  it "has a salary" do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    
+    expect(bobbi.salary).to eq(100000)
+  end
+
 
 
 
