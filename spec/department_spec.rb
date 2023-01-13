d705e370f0d8b7b7f44a7a5c35a5b578be3677c2
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:25:32 -0700

    Add a test for department class hire method

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -14,5 +14,19 @@ describe 'department' do
     expect(customer_service.employees).to eq([])
   end
 
+  it "it can hire employees" do
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
+
+    expect(customer_service.employees).to eq([])
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+
+    expect(customer_service.employees).to eq([bobbi, aaron])
+  end
+
+
 
 end
