f32fbaf4a3034cc1b1cc25a23660f02f10e94f42
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:31:24 -0700

    Add test for department class expense method

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -38,6 +38,19 @@ describe 'department' do
     expect(customer_service.expenses).to eq(0)
   end
 
+  it "it can add expenses" do
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+
+    customer_service.expense(100)
+    expect(customer_service.expenses).to eq(100)
+    customer_service.expense(25)    
+    expect(customer_service.expenses).to eq(125)
+  end
+
 
 
 
