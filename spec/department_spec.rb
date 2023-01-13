1d950ff1fdafef9f0cdbc08b73b15d6be824b688
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 09:20:05 -0700

    Add test for department employees instance variable

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -4,7 +4,14 @@ require './lib/employee'
 describe 'department' do
   it "exists" do
     customer_service = Department.new("Customer Service")
+    
     expect(customer_service).to be_an_instance_of(Department)
+  end
+
+  it "does not have employees by default" do
+    customer_service = Department.new("Customer Service")
+    
+    expect(customer_service.employees).to be([])
   end
 
 
